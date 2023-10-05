4face7fddb068f01b14cf275b740792a79a08266
Maxim Gabidullin <after@ya.ru>
2023-10-05 10:42:06 +0300

    вёрстка

--- public/js/common.js
+++ public/js/common.js
@@ -1,37 +1,47 @@
 let app = new Vue({
     el: '#app',
     data: {
-        name: 'John Smith',
-        place: 'Portland, Oregon, USA',
-        lang: 'English',
-        lang_pic: '/svg/en.svg',
+        user: {
+            photo: '/userpics/1.png',
+            name: 'John Smith',
+            place:
+                'Portland, Oregon, USA',
+            lang:
+                'English',
+            lang_pic:
+                '/svg/en.svg',
 
-        experience: [{
-            name: 'PHP',
-            experience: '6 year'
-        }, {
-            name: 'Ruby',
-            experience: '2 year'
-        }, {
-            name: 'Java Script',
-            experience: '4.5 years'
-        }],
+            experience:
+                [{
+                    name: 'PHP',
+                    experience: '6 year'
+                }, {
+                    name: 'Ruby',
+                    experience: '2 year'
+                }, {
+                    name: 'Java Script',
+                    experience: '4.5 years'
+                }],
 
-        portfolio: [{
-            name: 'Bootstrap 4 Material Design (Sample Link)',
-            href: '#'
-        }, {
-            name: 'Modern JavaScript stack',
-            href: '#'
-        }, {
-            name: 'Datepicker for twitter bootstrap',
-            href: '#'
-        }, {
-            name: 'Fast and reliable Bootstrap widgets in Angular',
-            href: '#'
-        }]
+            portfolio:
+                [{
+                    name: 'Bootstrap 4 Material Design (Sample Link)',
+                    href: '#'
+                }, {
+                    name: 'Modern JavaScript stack',
+                    href: '#'
+                }, {
+                    name: 'Datepicker for twitter bootstrap',
+                    href: '#'
+                }, {
+                    name: 'Fast and reliable Bootstrap widgets in Angular',
+                    href: '#'
+                }],
+            availability: 'Full-time',
+            environment: 'GitHub, Mac OSX',
+            amaizing: 'The only true wisdom is in knowing you know nothing...',
+            expectation: 'There is only one good, knowledge, and one evil, ignorance.'
+        }
     },
-    methods: {
-
-    }
+    methods: {}
 });

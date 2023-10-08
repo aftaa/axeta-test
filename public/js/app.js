c281c00c812650185adee03970f1cdb29a88431c
Maxim Gabidullin <after@ya.ru>
2023-10-08 15:21:42 +0300

    try-no-cache-ymap

--- public/js/app.js
+++ public/js/app.js
@@ -56,7 +56,7 @@ let app = new Vue({
                     app.place = app.candidate.place;
                     document.getElementById('app').style.display = 'block';
                     document.getElementById('loading').style.display = 'none';
-                    setTimeout(() => init(), 5000);
+                    setTimeout(() => init(), 3000);
                 })
                 .catch(() => {
                     document.getElementById('loading').style.display = 'none';

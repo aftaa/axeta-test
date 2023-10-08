356c1d0406ddce1ee664cfbdf24c910536b3349a
Maxim Gabidullin <max@data-files.ru>
2023-10-08 15:51:53 +0300

    ~init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 composer install
 docker compose up -d --build
+echo 'Wait...'
+sleep 3
 docker compose cp init/init-db.sh mysql:/
 docker compose cp init/init-db.sql mysql:/
+sleep 2
 docker compose exec mysql ./init-db.sh
 docker compose exec php bin/console doctrine:migrations:migrate
 docker compose exec php bin/console doctrine:fixtures:load --purger axeta --purge-with-truncate

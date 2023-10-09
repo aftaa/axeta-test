bc6418a955b04fa897b163b8c754a631452b54b4
Maxim Gabidullin <after@ya.ru>
2023-10-09 13:50:43 +0300

    ~form

--- src/Form/CandidatePhotoType.php
+++ src/Form/CandidatePhotoType.php
@@ -4,23 +4,29 @@ namespace App\Form;
 
 use App\Entity\Candidate;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class CandidatePhotoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('place')
-            ->add('photo')
-            ->add('code')
-            ->add('availability')
-            ->add('environment')
-            ->add('amaizing')
-            ->add('expectation')
-            ->add('lang')
+            ->add('photo', FileType::class, [
+                'mapped' => false,
+                'required' => true,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                        ]
+                    ])
+                ]
+            ])
         ;
     }
 

1bb4baabd6d3fefe745b8e48856e1d71ffb12bb7
Maxim Gabidullin <after@ya.ru>
2023-10-09 14:07:40 +0300

    ~api-candidate-controller

--- src/Controller/Api/CandidateController.php
+++ src/Controller/Api/CandidateController.php
@@ -5,11 +5,15 @@ namespace App\Controller\Api;
 use App\DTO\CandidateName;
 use App\DTO\CandidatePlace;
 use App\Entity\Candidate;
+use App\Form\CandidatePhotoType;
 use App\Repository\CandidateRepository;
+use Exception;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Annotation\Route;
@@ -110,5 +114,19 @@ class CandidateController extends AbstractController
         $candidate->setPlace($dto->getPlace());
         $repository->save($candidate, true);
         return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/candidate/photo/{id}', methods: ['POST'])]
+    public function postPhoto(Candidate $candidate, Request $request): JsonResponse
+    {
+        $form = $this->createForm(CandidatePhotoType::class, $candidate)
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            /** @var UploadedFile $file */
+            $file = $form->get('photo')->getData();
+            $filename = $this->getParameter('userpics_directory') . $candidate->getId() . '.' . $file->getClientOriginalExtension();
+            $file->move($filename);
+            $candidate->setPhoto($this->getParameter('userpics_uri_prefix') . $filename);
+        }
     }
 }
